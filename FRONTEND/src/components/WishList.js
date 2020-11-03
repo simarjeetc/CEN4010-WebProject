395cc7d4ba40123b57ca16cb6b34496e6aaf3d58
tbrum004 <71197981+tbrum004@users.noreply.github.com>
2020-11-03 16:52:40 -0500

    Update WishList.js

--- FRONTEND/src/components/WishList.js
+++ FRONTEND/src/components/WishList.js
@@ -2,28 +2,20 @@ import React, { Component, useState } from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
 
-var books = [];
+  function WishList(){ 
 
-function addtoWL() {
-  books.push();
-}
 
-function WishList(){ 
-  
   return(
     <div className = "WishList"> 
     <h1 className = "WishList-header">WishList</h1>
 
 
-    <h5> </h5>
+    <h5></h5>
     <h6>Continue browsing<a href="http://localhost:3000/"><button className = "">Homepage</button></a> </h6>  
-    
+   
     </div>
   )
 }
  
-
-
-
 
 export default WishList;
