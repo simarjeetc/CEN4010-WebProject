63232b3b2fcdef4d872549daf953c46de13d1cc0
Travis Brumfield <71197981+tbrum004@users.noreply.github.com>
2020-12-02 19:05:34 -0500

    Update WishList.js

--- FRONTEND/src/components/WishList.js
+++ FRONTEND/src/components/WishList.js
@@ -1,21 +1,83 @@
 import React, { Component, useState } from 'react';
-import '../App.css';
 import {Link} from 'react-router-dom';
-
-  function WishList(){ 
-
-
-  return(
-    <div className = "WishList"> 
-    <h1 className = "WishList-header">WishList</h1>
+import '../App.css';
+import axios from 'axios';
+import './BookDetails';
 
 
-    <h5></h5>
-    <h6>Continue browsing<a href="http://localhost:3000/"><button className = "">Homepage</button></a> </h6>  
+
+class WishList extends Component{
+
+     
+     state = {
+      _id : "",
+      bookid : "",
+      name : "",
+      image : "",
+      author : "",
+      price : "",
+      wishlists: [],
+      updatedWISHLIST: []
+    }
+  
+    
+    componentDidMount = () => {
+      this.getWishlist();
+      this.postWishlist();
+ };
+
+
+    getWishlist = () => {
+      axios.get('/wishlists')
+        .then((current) => {
+          const data = current.data;
+          this.setState({ wishlists: data });
+        });     
+    };
+
+    postWishlist = () => {
+      const data = 
+      axios.post('/wishlists', data)
+      .then((data) => {
+                  console.log(data);
+    })
+       .catch((err) => {
+                  console.log(err);
+       })
+      }
+
+
+
+    displayWishlist = (wishlists) => {
+      return wishlists.map((wishlist, index) => 
+     
+       <div key={index}> 
+          <img className= "product-image" src={wishlist.image} alt=""></img>
+          
+          <div className="product-name"> <Link to={'/product/'+[wishlist.bookid]}>{wishlist.name}</Link> </div>
+          <div className="product-name">{wishlist.author}</div>
+          <div className="product-price">{wishlist.price}</div> 
+        </div>
+  
+    );
+    };
+
    
-    </div>
-  )
-}
- 
+    
+   render(){
 
+      return(
+    
+    <div className = "WishList">
+        
+        <div>{this.displayWishlist(this.state.wishlists)}</div>
+       
+  
+        </div>
+        )
+      }
+    }
+
+ 
 export default WishList;
+
